9d80b5ff65c4a79c9c63dad2a2fc8d7fe7a74044
MISTERIO33797 <s33797@pjwstk.edu.pl>
2025-04-24 17:51:16 +0200

    commit_2

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void makeNoise() {
+        System.out.println("Some noise");
+    }
+
+    public void roam() {
+        System.out.println("Animal is roaming");
+    }
+}
